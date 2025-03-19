401f708a1fa2ca015ccd245881d49c45cb977154
devitfro <alinabogdanovaaaaa@gmail.com>
2025-03-19 15:58:23 +0200

    update

---
+++ jokeDLL/jokeDLL/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace jokeDLL
+{
+    public class JokeGenerator
+    {
+        private readonly List<string> Jokes = new List<string>
+    {
+        "Программист — это машина, преобразующая кофе в код.",
+        "В магазине: — У вас есть книги по депрессии? — Да, но они не продаются.",
+        "Почему программисты не любят природу? — В ней слишком много багов.",
+        "Алкоголь не решает проблем... но и вода тоже!",
+        "Какое любимое занятие айтишника? — Перезагружаться!"
+    };
+
+        private readonly Random Random = new Random();
+
+        public string GetRandomJoke()
+        {
+            int index = Random.Next(Jokes.Count);
+            return Jokes[index];
+        }
+    }
+}
---
+++ testJokeDLL/testJokeDLL/Program.cs
@@ -0,0 +1,13 @@
+﻿using jokeDLL;
+
+namespace testJokeDLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            JokeGenerator jokeGenerator = new JokeGenerator();
+            Console.WriteLine(jokeGenerator.GetRandomJoke());
+        }
+    }
+}
