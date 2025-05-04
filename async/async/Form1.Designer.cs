c84b4756b04e48734bfbd6fcdc395a10db59d869
devitfro <alinabogdanovaaaaa@gmail.com>
2025-05-05 01:10:51 +0300

    update

---
+++ async/async/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace async
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.textBoxWord = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(401, 91);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(104, 77);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "SEARCH";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(61, 99);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(213, 20);
+            this.textBoxPath.TabIndex = 2;
+            this.textBoxPath.Text = "Enter path";
+            // 
+            // textBoxWord
+            // 
+            this.textBoxWord.Location = new System.Drawing.Point(61, 148);
+            this.textBoxWord.Name = "textBoxWord";
+            this.textBoxWord.Size = new System.Drawing.Size(213, 20);
+            this.textBoxWord.TabIndex = 3;
+            this.textBoxWord.TabStop = false;
+            this.textBoxWord.Text = "Enter word";
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(61, 250);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(121, 97);
+            this.listView.TabIndex = 6;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.textBoxWord);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.buttonSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.TextBox textBoxWord;
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ async/async/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace async
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async Task SearchWordInFile(string path, string word)
+        {
+            listView.Items.Clear();
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File not found!");
+                return;
+            }
+
+            int count = await CountWordAsync(path, word);
+
+            listView.Invoke(new Action(() =>
+            {
+                listView.Items.Add($"Count: {count}");
+            }));
+
+            MessageBox.Show("Search complete!");
+        }
+
+        private async Task<int> CountWordAsync(string path, string word)
+        {
+            try
+            {
+                int count = 0;
+
+                // async чтение содержимого файла
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string content = await reader.ReadToEndAsync();
+
+                    // Count вхождений слова
+                    count = (content.Length - content.Replace(word, "").Length) / word.Length;
+                }
+
+                return count;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error reading file {path}: {ex.Message}");
+                return 0;
+            }
+        }
+
+
+        private async void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string path = textBoxPath.Text;
+            string word = textBoxWord.Text;
+
+            if (string.IsNullOrEmpty(path) || string.IsNullOrEmpty(word))
+            {
+                MessageBox.Show("Path and word cannot be empty!");
+                return;
+            }
+
+            try
+            {
+                // Запуск поиска в асинхронном режиме
+                await SearchWordInFile(path, word);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void listResults_SelectedIndexChanged(object sender, EventArgs e) { }
+    }
+}
